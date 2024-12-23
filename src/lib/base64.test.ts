04a78cdb6b25a0cc645eadd10aa6e85e7275de61
Peter Hirn <peter.hirn@phi.ag>
2024-12-23 22:24:08 +0100

    test: use fast-check for base64 tests

--- src/lib/base64.test.ts
+++ src/lib/base64.test.ts
@@ -1,3 +1,4 @@
+import fc from "fast-check";
 import { describe, expect, test } from "vitest";
 
 import {
@@ -11,26 +12,34 @@ import {
 
 describe("base64", () => {
   test("string", () => {
-    const value = "my test string";
-    const encoded = btoa(value);
-    expect(tryDecodeBase64(encoded)).toEqual(value);
+    fc.assert(
+      fc.property(fc.string(), (value) => {
+        const encoded = btoa(value);
+        expect(tryDecodeBase64(encoded)).toEqual(value);
+      })
+    );
   });
 
   test("string url", () => {
-    const value = "my test string";
-    const encoded = encodeBase64Url(value);
-    expect(tryDecodeBase64Url(encoded)).toEqual(value);
+    fc.assert(
+      fc.property(fc.string(), (value) => {
+        const encoded = encodeBase64Url(value);
+        expect(tryDecodeBase64Url(encoded)).toEqual(value);
+      })
+    );
   });
 
   test("array", () => {
-    const value = new Uint8Array([0, 1, 2, 3, 4, 5, 6, 7, 8, 9]);
-    const encoded = encodeBase64Array(value);
-    expect(tryDecodeBase64UrlArray(encoded)).toEqual(value);
+    fc.property(fc.uint8Array(), (value) => {
+      const encoded = encodeBase64Array(value);
+      expect(tryDecodeBase64UrlArray(encoded)).toEqual(value);
+    });
   });
 
   test("array url", () => {
-    const value = new Uint8Array([0, 1, 2, 3, 4, 5, 6, 7, 8, 9]);
-    const encoded = encodeBase64UrlArray(value);
-    expect(tryDecodeBase64UrlArray(encoded)).toEqual(value);
+    fc.property(fc.uint8Array(), (value) => {
+      const encoded = encodeBase64UrlArray(value);
+      expect(tryDecodeBase64UrlArray(encoded)).toEqual(value);
+    });
   });
 });
