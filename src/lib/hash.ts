a29f5b5f8e1bb2286a1585ddd0584627384413a4
Peter Hirn <peter.hirn@phi.ag>
2024-12-23 14:26:08 +0100

    chore: remove node hash functions

--- src/lib/hash.ts
+++ src/lib/hash.ts
@@ -1,40 +1,4 @@
-import { type Hash, createHash } from "node:crypto";
-import { createReadStream } from "node:fs";
-import { pipeline } from "node:stream/promises";
-
 import { bufferToHex } from "~/lib/hex";
-
-export const createSha256 = (): Hash => {
-  const hash = createHash("sha256");
-  hash.setEncoding("hex");
-  return hash;
-};
-
-export const sha256File = async (path: string): Promise<string> => {
-  const stream = createReadStream(path);
-  const hash = createSha256();
-
-  await pipeline(stream, hash);
-
-  hash.end();
-  return hash.read();
-};
-
-export const createSha1 = (): Hash => {
-  const hash = createHash("sha1");
-  hash.setEncoding("hex");
-  return hash;
-};
-
-export const sha1File = async (path: string): Promise<string> => {
-  const stream = createReadStream(path);
-  const hash = createSha1();
-
-  await pipeline(stream, hash);
-
-  hash.end();
-  return hash.read();
-};
 
 export type Algorithm = "SHA-1" | "SHA-256" | "SHA-384" | "SHA-512";
 
