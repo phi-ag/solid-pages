718d3f9032dba55e64a384911b8cfecca0253cba
Peter Hirn <peter.hirn@phi.ag>
2025-01-29 13:06:58 +0100

    chore(deps): update tailwind to v4

--- app.config.ts
+++ app.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig } from "@solidjs/start/config";
+import tailwindcss from "@tailwindcss/vite";
 
 import { version } from "./package.json";
 
@@ -11,6 +12,7 @@ export default defineConfig({
     compatibilityDate: "2024-11-03"
   },
   vite: {
+    plugins: [tailwindcss()],
     define: {
       "import.meta.env.VERSION": JSON.stringify(version),
       "import.meta.env.REVISION": JSON.stringify(revision)
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,5 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   darkMode: ["class", '[data-mode="dark"]'],
-  content: ["./src/**/*.{html,js,jsx,md,mdx,ts,tsx}"],
-  theme: {
-    extend: {}
-  },
-  plugins: []
+  content: ["./src/**/*.{html,js,jsx,md,mdx,ts,tsx}"]
 };
